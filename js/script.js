b16fe9f740569c0717aae0886cda1b0fe463bf3d
Abayomi Aishida <yomicorperate@gmail.com>
2020-12-02 20:09:05 +0100

    Returning checkbox to false when a nav link is clicked

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 const toggler = document.querySelector(".toggler");
+let ul = document.querySelector(".menu");
+
+console.log(ul);
+// ul = Array.from(ul);
+
+// ul.addEventListener("click", () => {
+//   console.log("clicked");
+// });
+
 let itemText = document
   .querySelector("#projects")
   .getElementsByClassName("item-text");
@@ -15,4 +24,12 @@ const logger = () => {
   });
 };
 
+const removeOverlay = (e) => {
+  if (e.target.classList.contains("li")) {
+    console.log("clicked");
+    toggler.checked = false;
+  }
+};
+
+ul.addEventListener("click", removeOverlay);
 toggler.addEventListener("change", logger);
