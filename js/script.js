90c66b605068bc907a96d0f55272a2a86ef545a8
Abayomi Aishida <yomicorperate@gmail.com>
2020-12-10 17:07:30 +0100

    Smooth scrolling effect with jquery and js

--- js/script.js
+++ js/script.js
@@ -24,5 +24,21 @@ const removeOverlay = (e) => {
   }
 };
 
+// Smooth Scrolling
+$("#menu a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
+  }
+});
+
 ul.addEventListener("click", removeOverlay);
 toggler.addEventListener("change", logger);
